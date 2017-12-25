7516cc6d00b164b64c7d172d087f77c11f20dcf2
whatsupmofo420@gmail.com <subhamoypaul@Subhamoys-Mac.local>
2017-12-25 10:30:53 +0530

    Initial Commit

---
+++ Travel Map II/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Travel Map II
+//
+//  Created by Subhamoy Paul on 12/25/17.
+//  Copyright © 2017 Subhamoy Paul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
